ea2465154a62aad9916aa9aada1ac72b1d1a88aa
face4 <s1611361@u.tsukuba.ac.jp>
2018-12-08 23:57:18 +0900

    12/08

--- ABC/115/D.cpp
+++ ABC/115/D.cpp
@@ -4,7 +4,7 @@ using namespace std;
 typedef long long ll;
 
 ll dfs(int n, ll x){
-    if(n == 0)  return 1;
+    if(n == 0 && x == 1)  return 1;
     if(x <= 1)  return 0;
 
     ll len = (1ll<<(n+2))-3;
@@ -13,9 +13,8 @@ ll dfs(int n, ll x){
     // 区間を完全に覆ってしまった時点で枝刈りをしないとdfsの呼び出しが指数的に爆発して死ぬ
     if(x == len)    return 1 + 2ll*dfs(n-1, center-2);
     
-    if(x < center)          return dfs(n-1, x-1);
-    else if(x == center)    return 1 + dfs(n-1, center-2);
-    else if(x > center)     return 1 + dfs(n-1, center-2) + dfs(n-1, x-1-center+1);
+    if(x < center)  return dfs(n-1, x-1);
+    else            return 1 + dfs(n-1, center-2) + dfs(n-1, x-1-center+1);
 }
 
 int main(){
